28ad4465afe5912002683d957a95b6cb891a98a9
Pascal Castéran <casteran.p@sfeir.com>
2020-06-06 17:00:24 +0200

    WIP - New sample GCP config

--- samples/gcp/service_accounts.tf
+++ samples/gcp/service_accounts.tf
@@ -1,5 +1,5 @@
 locals {
-  service_accounts_prefix = "sa-"
+  service_accounts_prefix = "sa-${var.env}"
 }
 
 // Use the old style v2.0 version that uses `count`.
@@ -10,18 +10,21 @@ module "service_accounts_v2" {
   prefix     = local.service_accounts_prefix
   names      = ["foo", "bar"]
   project_roles = [
-    "${var.project_id}=>roles/viewer",
+    "${var.project_id}=>roles/bigquery.metadataViewer",
+    "${var.project_id}=>roles/logging.viewer",
   ]
 }
 
 // Use the new style v3.0 version that uses `for_each`.
 module "service_accounts_v3" {
-  source     = "terraform-google-modules/service-accounts/google"
-  version    = "~> 3.0"
-  project_id = var.project_id
-  prefix     = local.service_accounts_prefix
-  names      = ["baz"]
+  source       = "terraform-google-modules/service-accounts/google"
+  version      = "~> 3.0"
+  project_id   = var.project_id
+  prefix       = local.service_accounts_prefix
+  names        = ["baz"]
+  description  = "Awesome service account"
+  display_name = "baz"
   project_roles = [
-    "${var.project_id}=>roles/viewer",
+    "${var.project_id}=>roles/pubsub.viewer",
   ]
 }
